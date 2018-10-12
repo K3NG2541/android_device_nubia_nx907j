80918148d8f5af24afc9b5160dc53fc2e55acc80
root <root@K3NG2541.localdomain>
2018-10-12 14:46:50 +0000

    Add basic board configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,31 +1,124 @@
-USE_CAMERA_STUB := true
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# inherit from the proprietary version
--include vendor/nubia/nx907j/BoardConfigVendor.mk
+LOCAL_PATH := device/xiaomi/mido
 
-TARGET_ARCH := arm
+TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_BOARD_PLATFORM := msm8953
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
+
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := MSM8953
 TARGET_NO_BOOTLOADER := true
-TARGET_BOARD_PLATFORM := unknown
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
-TARGET_CPU_SMP := true
-ARCH_ARM_HAVE_TLS_REGISTER := true
 
-TARGET_BOOTLOADER_BOARD_NAME := nx907j
+# Camera
+ USE_DEVICE_SPECIFIC_CAMERA := true
+ BOARD_QTI_CAMERA_32BIT_ONLY := true
+ TARGET_TS_MAKEUP := true
 
-BOARD_KERNEL_CMDLINE := 
-BOARD_KERNEL_BASE := 0x
-BOARD_KERNEL_PAGESIZE := 
+ # Charger
+ BOARD_CHARGER_ENABLE_SUSPEND := true
+ BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
-# fix this up by examining /proc/mtd on a running device
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00380000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00480000
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x08c60000
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x105c0000
-BOARD_FLASH_BLOCK_SIZE := 131072
+ # CMHW
+ BOARD_USES_CYANOGEN_HARDWARE := true
+ BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
 
-TARGET_PREBUILT_KERNEL := device/nubia/nx907j/kernel
+ # CNE / DPM
+ BOARD_USES_QCNE := true
+ TARGET_LDPRELOAD := libNimsWrap.so
 
-BOARD_HAS_NO_SELECT_BUTTON := true
+ # Crypto
+ TARGET_HW_DISK_ENCRYPTION := true
+
+ # Display
+ MAX_EGL_CACHE_KEY_SIZE := 12*1024
+ MAX_EGL_CACHE_SIZE := 2048*1024
+ NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
+ OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
+ TARGET_CONTINUOUS_SPLASH_ENABLED := true
+ TARGET_USES_C2D_COMPOSITION := true
+ TARGET_USES_ION := true
+ USE_OPENGL_RENDERER := true
+
+ # Filesystem
+ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+ TARGET_USERIMAGES_USE_EXT4 := true
+
+ # GPS
+ USE_DEVICE_SPECIFIC_GPS := true
+ TARGET_NO_RPC := true
+
+ # Keymaster
+ TARGET_PROVIDES_KEYMASTER := true
+
+ # Lights
+ TARGET_PROVIDES_LIBLIGHT := true
+
+ # Malloc
+ MALLOC_IMPL := dlmalloc
+
+ # Partitions (To-do Get actual partition size)
+ BOARD_BOOTIMAGE_PARTITION_SIZE := 0x04000000
+ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x04000000
+ BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
+ BOARD_USERDATAIMAGE_PARTITION_SIZE := 9999220736
+ BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+ BOARD_PERSISTIMAGE_PARTITION_SIZE := 33554432
+ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+
+ # Peripheral manager
+ TARGET_PER_MGR_ENABLED := true
+
+ # Power
+ TARGET_POWERHAL_VARIANT := qcom
+
+ # Properties
+ TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
+
+ # Qualcomm
+ BOARD_USES_QCOM_HARDWARE := true
+ BOARD_USES_QC_TIME_SERVICES := true
+
+ # RIL
+ TARGET_RIL_VARIANT := caf
+
+ # Recovery
+ TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
+
+ # SELinux
+ include device/qcom/sepolicy/sepolicy.mk
+ BOARD_SEPOLICY_DIRS += $(LOCAL_PATH)/sepolicy
+ 
+ # inherit from the proprietary version
+-include vendor/nubia/nx907j/BoardConfigVendor.mk
